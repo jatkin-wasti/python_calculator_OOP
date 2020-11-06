b437af7bb4216b959123f39fceec41ce1782902e
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-06 11:29:35 +0000

    added self parameter to calculator functions

--- calculator.py
+++ calculator.py
@@ -5,27 +5,27 @@ class Calculator:
         print("Welcome to the Calculator")
 
     # Creating a function that will add two arguments
-    def add(num1, num2):  # Takes two arguments
+    def add(self, num1, num2):  # Takes two arguments
         print(num1 + num2)  # Print the result
 
     # Creating a function that will subtract two arguments
-    def subtract(num1, num2):  # Defining the function with two arguments
+    def subtract(self, num1, num2):  # Defining the function with two arguments
         print(num1 - num2)  # Printing the value of one subtracted by the other
 
     # Create a function to multiply (*)
-    def multiply(num1, num2):
+    def multiply(self, num1, num2):
         return num1 * num2
 
     # Create a function to do division (/)
-    def divide(num1, num2):
+    def divide(self, num1, num2):
         return num1 / num2
 
     # Create a function to do modulo division (%)
-    def modulo(num1, num2):
+    def modulo(self, num1, num2):
         return num1 % num2
 
     # Create a function to do exponentials (^)
-    def exponent(num1, num2):
+    def exponent(self, num1, num2):
         result = num1  # Creating the variable we wish to print
         for _ in range(num2 - 1):  # This loop will run for the amount of times stored in the num2 variable
             result = result * num1  # Multiplies the current value of the calculation with its original value
